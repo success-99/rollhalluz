d833a2285ddb8a60754fd682cd79a67e3675cc97
mbp13 <shaxobiddina20@gmail.com>
2024-06-16 23:02:23 +0500

    user full validate

--- users/forms.py
+++ users/forms.py
@@ -13,9 +13,13 @@ def validate_numeric(value):
     if not value.isdigit():
         raise ValidationError("Telefon nomer raqamlardan iborat bo'lsin.")
 
+def validate_string(value):
+    if not value.isalpha():
+        raise ValidationError("Familiya va Ismingiz iborat bo'lsin.")
+
 
 class SignupForm(forms.Form):
-    full_name = forms.CharField(max_length=255, required=True)
+    full_name = forms.CharField(max_length=255, required=True, validators=[validate_string])
     birth_day = forms.IntegerField(min_value=7, max_value=15)
     phone = forms.CharField(min_length=9, max_length=9, required=True, validators=[validate_numeric], error_messages={
         'min_length': 'Telefon raqam kamida 9 ta belgidan iboratligiga ishonch hosil qiling.',
@@ -39,7 +43,7 @@ class SignupForm(forms.Form):
 
 
 class SignupForm1(forms.Form):
-    full_name = forms.CharField(max_length=255, required=True)
+    full_name = forms.CharField(max_length=255, required=True, validators=[validate_string])
     birth_day = forms.IntegerField(min_value=16, max_value=70)
     phone = forms.CharField(min_length=9, max_length=9, required=True, validators=[validate_numeric], error_messages={
             'min_length': 'Telefon raqam kamida 9 ta belgidan iboratligiga ishonch hosil qiling.',
